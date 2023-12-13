6fe34182ff653b26227d3482ddea4474494263b6
Mohamed zouine <mohamedzouine827@gmail.com>
2023-12-13 21:19:59 +0100

    monty

--- main.c
+++ main.c
@@ -1,28 +1,33 @@
 #include "monty.h"
-
-int main(int argc, char* argv[])
+/**
+ * main - Main entry point for Stacks and Queues
+ * @argc: Argument count for the matrix
+ * @argv: Argument vector for the matrix
+ * Return: 0 on success or other number if it fails
+ */
+int main(int argc, char *argv[])
 {
-    FILE *my_file;
-    char buffer[2048];
-    stack_t *head_node = NULL;
-    int line_number = 1, i;
+	FILE *my_file;
+	char buffer[2048];
+	stack_t *head_node = NULL;
+	int line_number = 1, i;
 
-    if (argc != 2)
-    {
-        fprintf(stderr, "USAGE: monty file\n");
-        exit(1);
-    }
-    my_file = fopen(argv[1], "r");
-    i = 0;
-    while(fgets(buffer, sizeof(buffer), my_file) != NULL)
-    {
-        for (; i < line_number; i++)
-        {
-            check_function(buffer, &head_node);
-        }
-        line_number++;
-    }
-    fclose(my_file);
-    free_stack(head_node);
-    return (0);
+	if (argc != 2)
+	{
+		fprintf(stderr, "USAGE: monty file\n");
+		exit(1);
+	}
+	my_file = fopen(argv[1], "r");
+	i = 0;
+	while (fgets(buffer, sizeof(buffer), my_file) != NULL)
+	{
+		for (; i < line_number; i++)
+		{
+			check_function(buffer, &head_node);
+		}
+		line_number++;
+	}
+	fclose(my_file);
+	free_stack(head_node);
+	return (0);
 }
--- read_file.c
+++ read_file.c
@@ -1,82 +1,102 @@
 #include "monty.h"
+/**
+ * push_to_stack - push element to the stack
+ * @value: value of the node
+ * @head: the head of the linked list
+ * Return: the head
+ */
 
 stack_t *push_to_stack(stack_t *head, char *value)
 {
-    stack_t *new = NULL;
-    int val;
+	stack_t *new = NULL;
+	int val;
 
-    val = atoi(value);   
-    new = malloc(sizeof(stack_t));
-    if (new == NULL)
-    {
-        exit(EXIT_FAILURE);
-    }
-    new->n = val;
-    new->prev = NULL;
-    new->next = head;
-    if (head)
-        head->prev = new;
-    head = new;
-    return (head);
+	val = atoi(value);
+	new = malloc(sizeof(stack_t));
+	if (new == NULL)
+	{
+		exit(EXIT_FAILURE);
+	}
+	new->n = val;
+	new->prev = NULL;
+	new->next = head;
+	if (head)
+		head->prev = new;
+	head = new;
+	return (head);
 }
-
+/**
+ * pall - print all
+ * @head: the head of the linked list
+ */
 
 void pall(stack_t *head)
 {
-    while(head != NULL)
-    {
-        printf("%d\n", head->n);
-        head = head->next;
-    }
+	while (head != NULL)
+	{
+		printf("%d\n", head->n);
+		head = head->next;
+	}
 }
+/**
+ * check_function - check the opcode
+ * @buffer: the buffer that contain the line
+ * @head: the head of the linked list
+ * Return: the head
+ */
 
 stack_t *check_function(char *buffer, stack_t **head)
 {
-    char *tokenize_value, *value;
+	char *tokenize_value, *value;
 
-    if (!buffer)
-        fprintf(stderr, "Error: malloc failed\n");
-    tokenize_value = strtok(buffer, " \n\t");
-    if (tokenize_value != NULL)
-    {
-        value = strtok(NULL, " ");
-    }
+	if (!buffer)
+		fprintf(stderr, "Error: malloc failed\n");
+	tokenize_value = strtok(buffer, " \n\t");
+	if (tokenize_value != NULL)
+	{
+		value = strtok(NULL, " ");
+	}
 
-    if (strcmp("push", tokenize_value) == 0)
-    {
-        *head = push_to_stack(*head, value);
-    }
+	if (strcmp("push", tokenize_value) == 0)
+	{
+		*head = push_to_stack(*head, value);
+	}
 
-    if (strcmp("pall", tokenize_value) == 0)
-    {
-        pall(*head);
-    }
-    if (strcmp("pint", tokenize_value) == 0)
-    {
-        pint(*head);
-    }
-    return (*head);
+	if (strcmp("pall", tokenize_value) == 0)
+	{
+		pall(*head);
+	}
+	if (strcmp("pint", tokenize_value) == 0)
+	{
+		pint(*head);
+	}
+	return (*head);
 }
-
+/**
+ * free_stack - free stack
+ * @node: the head of the linked list
+ */
 void free_stack(stack_t *node)
 {
-    stack_t *temp;
+	stack_t *temp;
 
-    while(node)
-    {
-        temp = node->next;
-        free(node);
-        node = temp;
-    }
+	while (node)
+	{
+		temp = node->next;
+		free(node);
+		node = temp;
+	}
 }
-
+/**
+ * pint - print element
+ * @head: the head of the linked list
+ */
 void pint(stack_t *head)
 {
-    int i = 1;
-    if (!head){
-        fprintf(stderr, "L%d: can't pint, stack empty\n");
-        exit(EXIT_FAILURE);
-    }
-    i++;
-    printf("%d\n", head->n);
+	if (!head)
+	{
+		fprintf(stderr, "L: can't pint, stack empty\n");
+		exit(EXIT_FAILURE);
+	}
+	printf("%d\n", head->n);
 }
